5af97248af366d1f2d183303a4cbaf85fc1a1237
Ankit <ankit.kumar10@dxc.com>
2019-11-05 12:18:14 -0800

    added vue build to backend

--- app.js
+++ app.js
@@ -76,9 +76,9 @@ next();
 });
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.use('/', express.static(path.resolve(__dirname, 'dist')));
 
 
 
